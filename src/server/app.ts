6b1068a2885d392386ae5443ee354922908d17fb
cardosofiles <cardosofiles@outlook.com>
2025-02-23 00:30:25 -0300

    feat: implementing API routes

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,13 +1,35 @@
-// libraries
+import fastifyCors from '@fastify/cors'
 import fastify from 'fastify'
+import {
+  type ZodTypeProvider,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 
-const app = fastify()
+import { createCompletionRoute } from '@/server/routes/create-completion'
+import { createGoalRoute } from '@/server/routes/create-goal'
+import { getPendingGoalsRoute } from '@/server/routes/get-pending-goals'
+// import { getWeekSummaryRoute } from '@/server/routes/get-week-summary'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createGoalRoute)
+app.register(getPendingGoalsRoute)
+app.register(createCompletionRoute)
+// app.register(getWeekSummaryRoute)
 
 app
   .listen({
-    port: 3000,
+    port: 3333,
     host: '0.0.0.0',
   })
   .then(() => {
-    console.log('🚀 Server listening on port 3333 🔥')
+    console.log('🚀 HTTP server listening 🔥')
   })
